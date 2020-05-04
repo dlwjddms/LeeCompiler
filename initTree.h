81a1941388673ef8eeb426d01470f4bddbf56af3
luminus7 <isangjin77@gmail.com>
2020-05-04 10:42:48 +0900

    add new tokenTree definition

--- initTree.h
+++ initTree.h
@@ -16,6 +16,16 @@ struct tokenTree* boHead ;
 struct tokenTree* fHead ;
 struct tokenTree* keyHead ;
 
+struct tokenTree* idHead ;
+struct tokenTree* btHead ;
+struct tokenTree* compHead ;
+struct tokenTree* assiHead ;
+struct tokenTree* aritHead ;
+struct tokenTree* terHead ;
+struct tokenTree* brcHead ;
+struct tokenTree* parHead ;
+struct tokenTree* sepHead ;
+
 void initTree();
 void freeTree();
 void initKeyword(); 
@@ -24,4 +34,13 @@ void initString();
 void initInteger();
 void initFloat();
 void initVarTree();
+void initIdentifier();
+void initBitwiseop();
+void initComparisonop();
+void initAssignop();
+void initArithop();
+void initTermin();
+void initBrace();
+void initParentheses();
+void initSeperator();
 	
